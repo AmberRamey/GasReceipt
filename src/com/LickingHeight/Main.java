2c07aa3baddce122a487cd92674d6e1113ae59d2
Amber <amber.ramey.2022@student.lhschools.org>
2020-03-06 12:02:55 -0500

    intelij commit

---
+++ src/com/LickingHeight/Main.java
@@ -0,0 +1,31 @@
+package com.LickingHeight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-----------------------------+");
+        System.out.println("|Amber's Gas Station          |");
+        System.out.println("|1789 Broad St                |");
+        System.out.println("|8937 Mink Rd                 |");
+        System.out.println("|Date               Time      |");
+        System.out.println("|09/SEP/2019        5:36 AM   |");
+        System.out.println("|Invoice#           74830583  |");
+        System.out.println("|AUTH#              00194603  |");
+        System.out.println("|Mastercard                   |");
+        System.out.println("|************63               |");
+        System.out.println("| Pump      Product     $/G   |");
+        System.out.println("| 05        Unleaded    $2.56 |");
+        System.out.println("|                             |");
+        System.out.println("| Amount          fuel Total  |");
+        System.out.println("| 25.00            $64.00     |");
+        System.out.println("|                             |");
+        System.out.println("|                             |");
+        System.out.println("|  Thank You for Shopping at  |");
+        System.out.println("|     Amber's Gas Station     |");
+        System.out.println("+-----------------------------+");
+
+    }
+
+}
+
+
